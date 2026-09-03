76ba1219ed6e9ae5c0d67ac9c97edf5456f3479d
tonystark7468 <75470984+tonystark7468@users.noreply.github.com>
2026-09-03 15:09:31 -0400

    Update firebase-config.js

--- docs/firebase-config.js
+++ docs/firebase-config.js
@@ -1,6 +1,6 @@
 // Config for the "poker-test-1-834c2" Firebase project.
 export const firebaseConfig = {
-  apiKey: "AIzaSyC1•••••••••••••••••••••••••••••••",
+  apiKey: "AIzaSyC1fhhVGXQSH7_whCK0YQT4S7U7RHZf8KU",
   authDomain: "poker-test-1-834c2.firebaseapp.com",
   projectId: "poker-test-1-834c2",
   storageBucket: "poker-test-1-834c2.firebasestorage.app",
